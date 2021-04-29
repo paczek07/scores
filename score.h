cb5716a3c6ef74fee619645acda4780de9c337ba
paczek07 <36909317+paczek07@users.noreply.github.com>
2021-04-29 17:39:14 +0300

    Delete score.h

--- score.h
+++
@@ -1,8 +0,0 @@
-
-//edited from github...
-#pragma once
-
-void input_scores(int scores[]);
-void evaluation(int scores[], int marks[]);
-void classification(int marks[], int statistics[]);
-void output_results(int marks[], int statistics[], int scores[]);
