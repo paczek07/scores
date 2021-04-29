e091029ead79c5290632b9dcf9894162d80d0b80
2000598 <e2000598@shell.vamk.fi>
2021-04-29 18:04:39 +0300

    scores.h

---
+++ score.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void input_scores(int scores[]);
+void evaluation(int scores[], int marks[]);
+void classification(int marks[], int statistics[]);
+void output_results(int marks[], int statistics[], int scores[]);
